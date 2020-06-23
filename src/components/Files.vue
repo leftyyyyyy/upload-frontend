960c2eb452a400c7365c3117e8299babc56745f1
leftyyyyyy <bandzbond@gmail.com>
2020-06-23 05:49:51 +0000

    fixed == sign

--- src/components/Files.vue
+++ src/components/Files.vue
@@ -73,7 +73,7 @@ export default {
             }
           })
           .then(response => {
-            if (!Object.keys(response.data).length === 0) {
+            if (!Object.keys(response.data).length == 0) {
               this.files.push(response.data);
               this.userMsg = "Successfully uploaded!";
             } else {
